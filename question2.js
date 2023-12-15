2f7c68234ead1e707e3e317251459b29321689c3
kentigeorge <kentigeorge567@gmail.com>
2023-12-15 12:42:48 +0100

    first commit

---
+++ question2.js
@@ -0,0 +1,25 @@
+//Data 2
+const data = [1,3,5,6,"hello", "guava", "hello world", 34,56 ,54]
+const numberArry = [453,656,67645,5476,798,5678,989,34545,6465768,79,76545364,75,8789,987,64765,]
+
+// #3I
+console.log(numberArry.sort(function(a, b)
+{
+    return a-b;
+}))
+
+// #3II
+data.forEach(index.name =
+    (
+        console.log(data)
+    ));
+
+    numberArry.forEach(element =>
+        {
+            console.log(element.item);
+        })
+
+
+let totalnum = numberArry.reduce ((element, total) => element + total)
+console.log(total)
+
